b0ce2328340135ac23df776a1a1db196a7d0f7e6
Matt Fuller <mnfuller552@gmail.com>
2023-03-09 15:12:22 -0500

    finished weekly challenges 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,49 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double subtract = minuend - subtrahend;
+            return subtract;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+           int Add = number1  + number2;
+            return Add;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+                return number1;
+            else
+                return number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long multiply = factor1 * factor2;
+            return multiply;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
-        }
+
+            if(nameOfPerson == "")
+            {
+                return "Hello!";
+
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+
+
+        }    
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
